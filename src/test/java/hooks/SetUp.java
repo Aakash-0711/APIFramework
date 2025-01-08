0b41d2ba11d06080eed3291337325cb07c64852b
Aakash-0711 <57639785+Aakash-0711@users.noreply.github.com>
2025-01-08 17:12:16 +0530

    Added SetUp class hooks package to centralize base URI

---
+++ src/test/java/hooks/SetUp.java
@@ -0,0 +1,33 @@
+package hooks;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+public class SetUp {
+	
+	@Before
+	public void setUp() throws FileNotFoundException, IOException{ 
+		Properties prop = new Properties();
+		prop.load(new FileInputStream(new File("./src/test/resources/config.properties")));
+		
+		RestAssured.baseURI = prop.getProperty("base.uri");
+		
+		
+	}
+	
+	@After
+	public void tearDown(Scenario sc){ 
+		System.out.println(sc.getName());
+	}
+}
